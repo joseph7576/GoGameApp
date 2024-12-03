8bd495ef30a6d0dd517d65241c9d2c91420b8f02
Joseph Hosseini <hosseini7576@outlook.com>
2024-12-03 14:29:32 +0330

    feat(user): create user service - register user case

---
+++ service/userservice/service.go
@@ -0,0 +1,67 @@
+package userservice
+
+import (
+	"GoGameApp/entity"
+	"GoGameApp/pkg/phonenumber"
+	"fmt"
+)
+
+type Repository interface {
+	IsPhoneNumberUnique(phoneNumber string) (bool, error)
+	CreateUser(u entity.User) (entity.User, error)
+}
+
+type Service struct {
+	repo Repository
+}
+
+type RegisterRequest struct {
+	Name        string
+	PhoneNumber string
+}
+
+type RegisterResponse struct {
+	User entity.User
+}
+
+func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
+	//TODO: verify phone number
+
+	// validate phone number
+	if !phonenumber.IsValid(req.PhoneNumber) {
+		return RegisterResponse{}, fmt.Errorf("phone number is not valid")
+	}
+
+	// check uniqueness of phone number
+	if isUnique, err := s.repo.IsPhoneNumberUnique(req.PhoneNumber); err != nil || !isUnique {
+		if err != nil {
+			return RegisterResponse{}, fmt.Errorf("unexpected error: %w", err)
+		}
+
+		if !isUnique {
+			return RegisterResponse{}, fmt.Errorf("phone number is not unique")
+		}
+	}
+
+	// validate name
+	if len(req.Name) < 3 {
+		return RegisterResponse{}, fmt.Errorf("name length should be greater than 3")
+	}
+
+	// create new user in storage
+	user := entity.User{
+		ID:          0,
+		PhoneNumber: req.PhoneNumber,
+		Name:        req.Name,
+	}
+
+	createdUser, err := s.repo.CreateUser(user)
+	if err != nil {
+		return RegisterResponse{}, fmt.Errorf("unexpected error: %w", err)
+	}
+
+	// return created user
+	return RegisterResponse{
+		User: createdUser,
+	}, nil
+}

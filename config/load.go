aed8d4d7a22c18dfb94bb874412220cbc581828c
Joseph Hosseini <hosseini7576@outlook.com>
2024-12-30 12:01:46 +0330

    fix(config/load): fix the env naming convention for underscore variables

--- config/load.go
+++ config/load.go
@@ -23,8 +23,10 @@ func Load(configPath string) *Config {
 	k.Load(file.Provider(configPath), yaml.Parser())
 
 	k.Load(env.Provider("GAMEAPP_", ".", func(s string) string {
-		return strings.Replace(strings.ToLower(
+		str := strings.Replace(strings.ToLower(
 			strings.TrimPrefix(s, "GAMEAPP_")), "_", ".", -1)
+
+		return strings.Replace(str, "..", "_", -1)
 	}), nil)
 
 	// Quick unmarshal.
